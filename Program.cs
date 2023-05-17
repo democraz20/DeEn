90e4d4584e4bdb9be913c53f03f7d8002503374f
democraz20 <mozartrasig@gmail.com>
2023-05-17 11:37:35 +0700

    gitignore

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace DeEn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                string logo = @"
+▓█████▄ ▓█████ ▓█████  ███▄    █ 
+▒██▀ ██▌▓█   ▀ ▓█   ▀  ██ ▀█   █ 
+░██   █▌▒███   ▒███   ▓██  ▀█ ██▒
+░▓█▄   ▌▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒
+░▒████▓ ░▒████▒░▒████▒▒██░   ▓██░
+ ▒▒▓  ▒ ░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ 
+ ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
+ ░ ░  ░    ░      ░      ░   ░ ░ 
+   ░       ░  ░   ░  ░         ░ 
+ ░                               
+";
+                Console.WriteLine(logo);
+            //program loom
+            while(true) {
+                Console.Write("Decrypt or Encrypt a file? (1/2) >");
+                var choice = Console.ReadKey();
+                switch (choice.KeyChar.ToString()) {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        Console.WriteLine("Exiting...");
+                        return;
+                }
+            }
         }
     }
 }
---
+++ src/Selected.cs
@@ -0,0 +1,10 @@
+namespace DeEn {
+    class Selected {
+        public static void Decrypt() {
+
+        }
+        public static void Encrypt() {
+            
+        }
+    }
+}
