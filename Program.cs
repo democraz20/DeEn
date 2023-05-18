5864165d73cf3e39087daee1f7616d04597af391
democraz20 <mozartrasig@gmail.com>
2023-05-18 20:40:30 +0700

    switch

--- Program.cs
+++ Program.cs
@@ -16,20 +16,28 @@ namespace DeEn // Note: actual namespace depends on the project name.
  ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
  ░ ░  ░    ░      ░      ░   ░ ░ 
    ░       ░  ░   ░  ░         ░ 
- ░                               
 ";
                 Console.WriteLine(logo);
             //program loom
             while(true) {
                 Console.Write("Decrypt or Encrypt a file? (1/2) >");
                 var choice = Console.ReadKey();
+                Console.WriteLine();
                 switch (choice.KeyChar.ToString()) {
                     case "1":
+                        try {
+                            Selected.Decrypt();
+                        } catch (Exception e) {
+                            var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(f);
+                            Console.ResetColor();
+                        }
                         break;
                     case "2":
                         break;
                     default:
-                        Console.WriteLine("Exiting...");
+                        Console.WriteLine("\nExiting...");
                         return;
                 }
             }
--- src/Selected.cs
+++ src/Selected.cs
@@ -1,7 +1,11 @@
 namespace DeEn {
     class Selected {
         public static void Decrypt() {
-
+            Console.Write("Enter file to decrypt> ");
+            string? filename = Console.ReadLine();
+            if (filename == null || filename == "")  {
+                throw new FileNotFoundException("file name cannot be empty!");
+            }
         }
         public static void Encrypt() {
             
