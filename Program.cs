2654deb94b5550b35b9f7f4305f6b79bb3df893c
democraz20 <mozartrasig@gmail.com>
2023-05-22 00:01:00 +0700

    to BitArray

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace DeEn // Note: actual namespace depends on the project name.
 {
@@ -6,49 +7,54 @@ namespace DeEn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-                string logo = @"
-▓█████▄ ▓█████ ▓█████  ███▄    █ 
-▒██▀ ██▌▓█   ▀ ▓█   ▀  ██ ▀█   █ 
-░██   █▌▒███   ▒███   ▓██  ▀█ ██▒
-░▓█▄   ▌▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒
-░▒████▓ ░▒████▒░▒████▒▒██░   ▓██░
- ▒▒▓  ▒ ░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ 
- ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
- ░ ░  ░    ░      ░      ░   ░ ░ 
-   ░       ░  ░   ░  ░         ░ 
-";
-                Console.WriteLine(logo);
-            //program loom
-            while(true) {
-                Console.Write("Decrypt or Encrypt a file? (1/2) >");
-                var choice = Console.ReadKey();
-                Console.WriteLine();
-                switch (choice.KeyChar.ToString()) {
-                    case "1":
-                        try {
-                            Selected.Decrypt();
-                        } catch (Exception e) {
-                            var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(f);
-                            Console.ResetColor();
-                        }
-                        break;
-                    case "2":
-                        try {
-                            Selected.Encrypt();
-                        } catch (Exception e) {
-                            var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(f);
-                            Console.ResetColor();
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("\nExiting...");
-                        return;
-                }
-            }
+            string path = @"bin\Debug\net7.0\DeEn.dll";
+            byte[] bytes = File.ReadAllBytes(path);
+
+            var Bits = new BitArray(bytes);
+            Console.WriteLine(Bits);
+//                 string logo = @"
+// ▓█████▄ ▓█████ ▓█████  ███▄    █ 
+// ▒██▀ ██▌▓█   ▀ ▓█   ▀  ██ ▀█   █ 
+// ░██   █▌▒███   ▒███   ▓██  ▀█ ██▒
+// ░▓█▄   ▌▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒
+// ░▒████▓ ░▒████▒░▒████▒▒██░   ▓██░
+//  ▒▒▓  ▒ ░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ 
+//  ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
+//  ░ ░  ░    ░      ░      ░   ░ ░ 
+//    ░       ░  ░   ░  ░         ░ 
+// ";
+//                 Console.WriteLine(logo);
+//             //program loom
+//             while(true) {
+//                 Console.Write("Decrypt or Encrypt a file? (1/2) >");
+//                 var choice = Console.ReadKey();
+//                 Console.WriteLine();
+//                 switch (choice.KeyChar.ToString()) {
+//                     case "1":
+//                         try {
+//                             Selected.Decrypt();
+//                         } catch (Exception e) {
+//                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
+//                             Console.ForegroundColor = ConsoleColor.Red;
+//                             Console.WriteLine(f);
+//                             Console.ResetColor();
+//                         }
+//                         break;
+//                     case "2":
+//                         try {
+//                             Selected.Encrypt();
+//                         } catch (Exception e) {
+//                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
+//                             Console.ForegroundColor = ConsoleColor.Red;
+//                             Console.WriteLine(f);
+//                             Console.ResetColor();
+//                         }
+//                         break;
+//                     default:
+//                         Console.WriteLine("\nExiting...");
+//                         return;
+//                 }
+//             }
         }
     }
 }
