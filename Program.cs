529b10b309fdd7ffcc351f79b9f7722414a0cd9d
democraz20 <mozartrasig@gmail.com>
2023-05-25 00:01:00 +0700

    base64 based dictionary

--- Program.cs
+++ Program.cs
@@ -28,56 +28,76 @@ namespace DeEn // Note: actual namespace depends on the project name.
             var Bits = new BitArray(bytes);
 
             List<bool> bitlist = new List<bool>();
-            foreach (bool bit in Bits) {
+            foreach (bool bit in Bits)
+            {
                 bitlist.Add(bit);
             }
-            
+
             List<List<bool>> chunks = bitlist.Chunk(6).ToList();
 
-            Console.WriteLine(Bits);
-//                 string logo = @"
-// ▓█████▄ ▓█████ ▓█████  ███▄    █ 
-// ▒██▀ ██▌▓█   ▀ ▓█   ▀  ██ ▀█   █ 
-// ░██   █▌▒███   ▒███   ▓██  ▀█ ██▒
-// ░▓█▄   ▌▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒
-// ░▒████▓ ░▒████▒░▒████▒▒██░   ▓██░
-//  ▒▒▓  ▒ ░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ 
-//  ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
-//  ░ ░  ░    ░      ░      ░   ░ ░ 
-//    ░       ░  ░   ░  ░         ░ 
-// ";
-//                 Console.WriteLine(logo);
-//             //program loom
-//             while(true) {
-//                 Console.Write("Decrypt or Encrypt a file? (1/2) >");
-//                 var choice = Console.ReadKey();
-//                 Console.WriteLine();
-//                 switch (choice.KeyChar.ToString()) {
-//                     case "1":
-//                         try {
-//                             Selected.Decrypt();
-//                         } catch (Exception e) {
-//                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
-//                             Console.ForegroundColor = ConsoleColor.Red;
-//                             Console.WriteLine(f);
-//                             Console.ResetColor();
-//                         }
-//                         break;
-//                     case "2":
-//                         try {
-//                             Selected.Encrypt();
-//                         } catch (Exception e) {
-//                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
-//                             Console.ForegroundColor = ConsoleColor.Red;
-//                             Console.WriteLine(f);
-//                             Console.ResetColor();
-//                         }
-//                         break;
-//                     default:
-//                         Console.WriteLine("\nExiting...");
-//                         return;
-//                 }
-//             }
+            var base64table = new Dictionary<int, char>(){
+                { 0, 'A' }, { 1, 'B' }, { 2, 'C' }, { 3, 'D' },
+            { 4, 'E' }, { 5, 'F' }, { 6, 'G' }, { 7, 'H' },
+            { 8, 'I' }, { 9, 'J' }, { 10, 'K' }, { 11, 'L' },
+            { 12, 'M' }, { 13, 'N' }, { 14, 'O' }, { 15, 'P' },
+            { 16, 'Q' }, { 17, 'R' }, { 18, 'S' }, { 19, 'T' },
+            { 20, 'U' }, { 21, 'V' }, { 22, 'W' }, { 23, 'X' },
+            { 24, 'Y' }, { 25, 'Z' }, { 26, 'a' }, { 27, 'b' },
+            { 28, 'c' }, { 29, 'd' }, { 30, 'e' }, { 31, 'f' },
+            { 32, 'g' }, { 33, 'h' }, { 34, 'i' }, { 35, 'j' },
+            { 36, 'k' }, { 37, 'l' }, { 38, 'm' }, { 39, 'n' },
+            { 40, 'o' }, { 41, 'p' }, { 42, 'q' }, { 43, 'r' },
+            { 44, 's' }, { 45, 't' }, { 46, 'u' }, { 47, 'v' },
+            { 48, 'w' }, { 49, 'x' }, { 50, 'y' }, { 51, 'z' },
+            { 52, '0' }, { 53, '1' }, { 54, '2' }, { 55, '3' },
+            { 56, '4' }, { 57, '5' }, { 58, '6' }, { 59, '7' },
+            { 60, '8' }, { 61, '9' }, { 62, '+' }, { 63, '/' }
+            };
+
+
+            //                 string logo = @"
+            // ▓█████▄ ▓█████ ▓█████  ███▄    █ 
+            // ▒██▀ ██▌▓█   ▀ ▓█   ▀  ██ ▀█   █ 
+            // ░██   █▌▒███   ▒███   ▓██  ▀█ ██▒
+            // ░▓█▄   ▌▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒
+            // ░▒████▓ ░▒████▒░▒████▒▒██░   ▓██░
+            //  ▒▒▓  ▒ ░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ 
+            //  ░ ▒  ▒  ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░
+            //  ░ ░  ░    ░      ░      ░   ░ ░ 
+            //    ░       ░  ░   ░  ░         ░ 
+            // ";
+            //                 Console.WriteLine(logo);
+            //             //program loom
+            //             while(true) {
+            //                 Console.Write("Decrypt or Encrypt a file? (1/2) >");
+            //                 var choice = Console.ReadKey();
+            //                 Console.WriteLine();
+            //                 switch (choice.KeyChar.ToString()) {
+            //                     case "1":
+            //                         try {
+            //                             Selected.Decrypt();
+            //                         } catch (Exception e) {
+            //                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
+            //                             Console.ForegroundColor = ConsoleColor.Red;
+            //                             Console.WriteLine(f);
+            //                             Console.ResetColor();
+            //                         }
+            //                         break;
+            //                     case "2":
+            //                         try {
+            //                             Selected.Encrypt();
+            //                         } catch (Exception e) {
+            //                             var f = String.Format("An Exception occured while decrypting :\n {0}", e.Message);
+            //                             Console.ForegroundColor = ConsoleColor.Red;
+            //                             Console.WriteLine(f);
+            //                             Console.ResetColor();
+            //                         }
+            //                         break;
+            //                     default:
+            //                         Console.WriteLine("\nExiting...");
+            //                         return;
+            //                 }
+            //             }
         }
     }
 }
