2d687053499963759d68f4f2cae04d8d9c2c87df
democraz20 <mozartrasig@gmail.com>
2023-05-27 00:01:00 +0700

    moved to seperated function

--- Program.cs
+++ Program.cs
@@ -22,53 +22,10 @@ namespace DeEn // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(en(".gitignore"));
             // string path = @"bin\Debug\net7.0\DeEn.dll";
-            string path = @"testchar.txt";
-            byte[] bytes = File.ReadAllBytes(path);
-
-            var Bits = new BitArray(bytes);
-
-            List<bool> bitlist = new List<bool>();
-            foreach (bool bit in Bits)
-            {
-                bitlist.Add(bit);
-            }
-
-            List<List<bool>> chunks = bitlist.Chunk(6).ToList();
-
-            var base64table = new Dictionary<int, char>(){
-                { 0, 'A' }, { 1, 'B' }, { 2, 'C' }, { 3, 'D' },
-            { 4, 'E' }, { 5, 'F' }, { 6, 'G' }, { 7, 'H' },
-            { 8, 'I' }, { 9, 'J' }, { 10, 'K' }, { 11, 'L' },
-            { 12, 'M' }, { 13, 'N' }, { 14, 'O' }, { 15, 'P' },
-            { 16, 'Q' }, { 17, 'R' }, { 18, 'S' }, { 19, 'T' },
-            { 20, 'U' }, { 21, 'V' }, { 22, 'W' }, { 23, 'X' },
-            { 24, 'Y' }, { 25, 'Z' }, { 26, 'a' }, { 27, 'b' },
-            { 28, 'c' }, { 29, 'd' }, { 30, 'e' }, { 31, 'f' },
-            { 32, 'g' }, { 33, 'h' }, { 34, 'i' }, { 35, 'j' },
-            { 36, 'k' }, { 37, 'l' }, { 38, 'm' }, { 39, 'n' },
-            { 40, 'o' }, { 41, 'p' }, { 42, 'q' }, { 43, 'r' },
-            { 44, 's' }, { 45, 't' }, { 46, 'u' }, { 47, 'v' },
-            { 48, 'w' }, { 49, 'x' }, { 50, 'y' }, { 51, 'z' },
-            { 52, '0' }, { 53, '1' }, { 54, '2' }, { 55, '3' },
-            { 56, '4' }, { 57, '5' }, { 58, '6' }, { 59, '7' },
-            { 60, '8' }, { 61, '9' }, { 62, '+' }, { 63, '/' }
-            };
-
-            foreach (List<bool> boolList in chunks)
-            {
-                int result = 0;
-                for (int i = 0; i < boolList.Count; i++)
-                {
-                    bool bit = boolList[i];
-                    if (bit)
-                    {
-                        result |= 1 << i;
-                    }
-                }
-                Console.Write(base64table[result]);
-            }
-
+            
 
             //                 string logo = @"
             // ▓█████▄ ▓█████ ▓█████  ███▄    █ 
@@ -113,6 +70,56 @@ namespace DeEn // Note: actual namespace depends on the project name.
             //                         return;
             //                 }
             //             }
+        }
+
+        static string en(string filename) {
+            byte[] bytes = File.ReadAllBytes(filename);
+
+            var Bits = new BitArray(bytes);
+
+            List<bool> bitlist = new List<bool>();
+            foreach (bool bit in Bits)
+            {
+                bitlist.Add(bit);
+            }
+
+            List<List<bool>> chunks = bitlist.Chunk(6).ToList();
+
+            var base64table = new Dictionary<int, char>(){
+                { 0, 'A' }, { 1, 'B' }, { 2, 'C' }, { 3, 'D' },
+            { 4, 'E' }, { 5, 'F' }, { 6, 'G' }, { 7, 'H' },
+            { 8, 'I' }, { 9, 'J' }, { 10, 'K' }, { 11, 'L' },
+            { 12, 'M' }, { 13, 'N' }, { 14, 'O' }, { 15, 'P' },
+            { 16, 'Q' }, { 17, 'R' }, { 18, 'S' }, { 19, 'T' },
+            { 20, 'U' }, { 21, 'V' }, { 22, 'W' }, { 23, 'X' },
+            { 24, 'Y' }, { 25, 'Z' }, { 26, 'a' }, { 27, 'b' },
+            { 28, 'c' }, { 29, 'd' }, { 30, 'e' }, { 31, 'f' },
+            { 32, 'g' }, { 33, 'h' }, { 34, 'i' }, { 35, 'j' },
+            { 36, 'k' }, { 37, 'l' }, { 38, 'm' }, { 39, 'n' },
+            { 40, 'o' }, { 41, 'p' }, { 42, 'q' }, { 43, 'r' },
+            { 44, 's' }, { 45, 't' }, { 46, 'u' }, { 47, 'v' },
+            { 48, 'w' }, { 49, 'x' }, { 50, 'y' }, { 51, 'z' },
+            { 52, '0' }, { 53, '1' }, { 54, '2' }, { 55, '3' },
+            { 56, '4' }, { 57, '5' }, { 58, '6' }, { 59, '7' },
+            { 60, '8' }, { 61, '9' }, { 62, '+' }, { 63, '/' }
+            };
+
+            string s = "";
+            foreach (List<bool> boolList in chunks)
+            {
+                int result = 0;
+                for (int i = 0; i < boolList.Count; i++)
+                {
+                    bool bit = boolList[i];
+                    if (bit)
+                    {
+                        result |= 1 << i;
+                    }
+                }
+                s = s + base64table[result];
+                // Console.Write(base64table[result]);
+            }
+            return s;
         }
     }
 }
